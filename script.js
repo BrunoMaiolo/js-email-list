251d7d14246f9a7aa4044b1c8bcd7844415beff7
BrunoMaiolo <brunosj@hotmail.it>
2026-01-22 15:51:40 +0100

    Stampa di una mail in pagina tramite il DOM (in questo caso un "li" vuoto quindi un punto)

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-//Verifica funzionamento AXIOS//
+//Stampa di una mail in pagina tramite il DOM (in questo caso un "li" vuoto quindi un punto)//
+const emailList = document.getElementById('email-list');
+
 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
 .then(response => {
-    console.log(response.data.response);
+const email = response.data.response;
+
+const li = document.createElement('li');
+li.innerText == email;
+
+emailList.appendChild(li);
 });
