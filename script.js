73bae502022d0a27251fb2a5dd4a35b2e5f2c57f
BrunoMaiolo <brunosj@hotmail.it>
2026-01-22 16:15:17 +0100

    funzione per svuotare la lista email

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 //elemeneti dal dom//
 const emailList = document.getElementById('email-list');
 const button = docuument.getElementById('generate');
+//funzione per svuotare la lista email//
+function generateEmails() {
+    emailList.innerHTML = '';
+
 //Ciclo FOR per 10 email assegnando le email//
 for (let i = 0; i < 10; i++){
 
@@ -13,4 +17,5 @@ li.innerText = email;
 
 emailList.appendChild(li);
 });
+}
 }
